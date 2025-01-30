f2261b83e9b4c2f0c69e45f3e0268936cc697c40
orkunds <98600000+orkunds@users.noreply.github.com>
2025-01-30 21:07:58 +0300

    Add files via upload

---
+++ streamlit_app_döngülü.py
@@ -0,0 +1,140 @@
+from acoustics.signal import *
+from acoustics.bands import *
+from acoustics.standards.iso_tr_25417_2007 import REFERENCE_PRESSURE
+from acoustics.standards.iec_61672_1_2013 import (NOMINAL_OCTAVE_CENTER_FREQUENCIES,NOMINAL_THIRD_OCTAVE_CENTER_FREQUENCIES)
+
+import streamlit as st                                                    
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from scipy.optimize import curve_fit
+from sklearn.linear_model import LinearRegression 
+import os
+from time import localtime, strftime
+#from scipy.interpolate import interp1d
+
+data=pd.read_csv('Alpha_2.txt')
+fs= 50000
+####window_size= fs/20
+
+# bu fonksiyonda ses sinyalini frekans domaininde analiz edilir ve farklı frekans bantlarında sesin güç değerleri hesaplanır.
+def third_octaves2(p, window_size, density=False,frequencies=NOMINAL_THIRD_OCTAVE_CENTER_FREQUENCIES,ref=REFERENCE_PRESSURE):
+    #fob, level = third_octaves2(np.array(channelData[i:i+int(frameSize)]), sampleRate1, False, THIRD_OCTAVE_CENTER_FREQUENCIES)
+    
+    """Calculate level per 1/3-octave in frequency domain using the FFT.
+    :param x: Instantaneous signal :math:`x(t)`.
+    :param fs: Sample frequency.
+    :param density: Power density instead of power.
+    :returns: Tuple. First element is an instance of :class:`OctaveBand`. The second element an array.
+    .. note:: Based on power spectrum (FFT)
+    .. seealso:: :attr:`acoustics.bands.THIRD_OCTAVE_CENTER_FREQUENCIES`
+    .. note:: Exact center frequencies are always calculated.
+    """
+
+    #fs=window_size
+    fob = OctaveBand(center=frequencies, fraction=3)
+    f, p = power_spectrum(p, fs)
+    fnb = EqualBand(f)
+    power = integrate_bands(p, fnb, fob ) 
+    if density:
+        power /= (fob.bandwidth/fnb.bandwidth)
+    level = 10.0*np.log10(power / ref**2.0) #hesaplanan güçler kullanılarak seviyeler hesaplanıyo ve dB cinsine dönüştürülüyo.
+    return fob, level
+
+#fob,level= third_octaves2(data['basinc'], window_size, density=False,frequencies=NOMINAL_THIRD_OCTAVE_CENTER_FREQUENCIES,ref=REFERENCE_PRESSURE)
+#print(fob,level)
+
+
+time_max = int(data['zaman'].max())   # data olarak oluşturulan data frameden veri seçiliyo burda mesela zaman sütunu şeçildi 
+# bu sütunun en büyük değeri alındı ses sinyalinin uygulama süresi belirlendi
+
+TDP= int(fs*time_max)
+print("total data point=",TDP)
+
+data['basinc_yeni']= data['basinc'][0:TDP] # analizin daha hızlı olması için belirli bir bölümü kullanıldı datanın
+
+bands = {}
+center_freq=[400,500,630,800,1000,1250,1600,2000,2500,3150,4000,5000,6300,8000,10000]
+#center_freq=[1000]
+t60_degerleri= []
+for center_freq in center_freq: # center_freqi frequencies içine yazıyoruz
+    bands[str(center_freq)] = []
+
+    for keys, values in bands.items():
+        #window_size =int(keys)*2 
+        window_size = 3000  
+        step = int(window_size / 2)
+        delta_t = time_max / (TDP / (window_size) * 2)
+        times = np.linspace(0, time_max, num=int(time_max / delta_t))
+
+        for i in range(0, TDP + step, step):
+            fob, level = third_octaves2(data['basinc_yeni'][i:int(window_size) + i], fs, density=False,frequencies=center_freq, ref=REFERENCE_PRESSURE)
+            bands[keys].append(level)
+
+    # neden bu saniyeleri aldık?
+    saniye_3 = (len(bands[keys]) / time_max) * 3  # upper lower degerlerini görmek istiyosak saniyeleri dışarı aldım for keysin altına
+    saniye_6 = (len(bands[keys]) / time_max) * 6
+    selected_value = bands[keys][int(saniye_3):int(saniye_6)]
+    average_level = np.mean(selected_value)
+    upper_point = average_level - 5  # üst limit hesabı
+    #print(f"bant degeri: {keys}, upper point degeri: {upper_point}")
+    #plt.plot(times, bands['10000'][:len(times)])
+
+    saniye_12 = (len(bands[keys]) / time_max) * 12
+    saniye_14 = (len(bands[keys]) / time_max) * 14
+    selected_value2 = bands[keys][int(saniye_12):int(saniye_14)]
+    lower_point = np.nanmean(selected_value2)
+    #print(f"bant degeri: {keys}, lower point: {lower_point}")
+
+    band_data = bands[keys][:len(times)]
+    df = pd.DataFrame({'times': times, f'band_{keys}_level': band_data})
+    #print(df)
+
+    def mainn():
+        st.title("Akustik Analiz ! ")
+
+        #for center_freq in center_freq_list:  # center_freq_list, tüm merkez frekanslarının bir listesi
+            #keys = str(center_freq)
+
+        if upper_point - 30 > lower_point:
+            filtered_values = df[(df['times'] >= 6) & (df['times'] <= 12) & (df[f'band_{keys}_level'] < upper_point) & (df[f'band_{keys}_level'] > upper_point - 30)]
+
+            x_verileri = np.array(filtered_values['times']).reshape(-1, 1)
+            y_verileri = np.array(filtered_values[f'band_{keys}_level']).reshape(-1, 1)
+
+            reg_model = LinearRegression()
+            reg_model.fit(x_verileri, y_verileri)
+            predict_y = reg_model.predict(x_verileri)
+
+            a = reg_model.coef_[0][0]
+            b = reg_model.intercept_[0]
+
+            t_ilk = (upper_point - b) / a
+            t_son = (upper_point - 60 - b) / a
+            T60 = (t_son - t_ilk) * 1000
+            t60_degerleri.append({'merkez frekansları': center_freq, 'boş kabin T60 değerleri': T60})
+            
+            #def create_graph():
+            # grafiği arayüzde gösteriyo
+            st.subheader(f"Curve Fit Grafigi - Bant: {keys}")
+            fig, ax = plt.subplots()
+            ax.plot(df['times'], df[f'band_{keys}_level'])
+            ax.scatter(filtered_values['times'], filtered_values[f'band_{keys}_level'], color='red')
+            ax.plot(x_verileri, predict_y, color='yellow')
+            ax.set_xlabel('Zaman')
+            ax.set_ylabel('Seviye')
+            ax.axhline(upper_point, color='red', linestyle='--')
+            ax.axhline(lower_point, color='blue', linestyle='--')
+            st.pyplot(fig)
+            #st.plotly_chart(fig)
+
+            st.subheader(f"Sonuçlar - Bant: {keys}, T60 Degeri: {T60:.2f} milisaniye")
+            #st.write(f"T60 Degeri: {T60:.2f} milisaniye")  
+            #create_graph()   
+        else:
+            print(f'The Test Is Not Okay For {keys}')
+            st.write(f'Test Is Not Okay For {keys}')  
+    #df_t60 = pd.DataFrame(t60_degerleri)           
+    if __name__ == "__main__":
+        mainn()
---
+++ web_work.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 17:29:37 2025
+
+@author: lenovo
+"""
+
+import streamlit as st
+st.set_page_config(page_title="Akustik Analiz Uygulamaları", page_icon=":bar_chart:", layout="wide")
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+from datetime import datetime
+from streamlit_app_döngülü import mainn
+from utils import df_t60, df_t60_dolu, yutum_katsayisi
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+col1, col2 = st.columns([1, 0.5])
+with col1:
+    st.markdown("<h1 style='font-weight: bold; font-size: 46px;'>Akustik Analiz Uygulamalari</h1>", 
+                unsafe_allow_html=True)
+with col2:
+    st.image("https://static.wixstatic.com/media/349070_f1acaa38d241433d82ef28801913235c~mv2.png/v1/fill/w_251,h_68,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/349070_f1acaa38d241433d82ef28801913235c~mv2.png")    
+    
+def generate_pdf(graph_shown): # pdf oluÅturmak iÃ§in fonksiyon
+    c = canvas.Canvas("rapor.pdf", pagesize=letter)
+    #if graph_shown:
+    #    graph = mainn() #mainn fonk Ã§aÄÄ±rarak grafiÄi Ã§ektik
+    #    plt.savefig("graph.png") #grafiÄi png olarak kaydediyo
+    #    c.drawInlineImage("graph.png", 100, 380, width=400, height=300)
+
+    plt.savefig("yutum_katsayisi.png")  # yutum katsayÄ±sÄ± grafiÄini png olarak kaydediyoruz
+    c.drawInlineImage("yutum_katsayisi.png", 100, 350, width=400, height=300)  # PDF'e grafiÄi ekliyoruz
+    c.drawString(100, 750, "Akustik Analiz Raporu")
+    c.drawString(100, 700, f"Test Talep Eden/ Firma: {st.session_state.firma}")
+    c.drawString(100, 680, f"Malzeme Ismi: {st.session_state.malzeme}")
+    c.drawString(100, 660, f"Silinen Deney Tarihi: {st.session_state.test_date}")
+    #c.drawString(100, 640, f"Dolu Kabin Ä°Ã§in T60 DeÄerleri: {st.table(df_t60_dolu)}")
+
+    c.showPage()
+    c.save()    
+
+
+def main():
+    st.sidebar.title("Akustik Analiz Menüsü")
+    menu = ["Ana Sayfa", "Test","Test_Overall","Rapor"]
+    choice = st.sidebar.selectbox("Menü", menu)
+    
+    if choice == "Ana Sayfa":
+        st.markdown("<h1 style='font-size: 28px;'>Ana Sayfa <span style='font-size: 28px;'>🏠 </span></h1>", unsafe_allow_html=True)
+        st.write("""Bu web uygulaması akustik veri analizi yapılması ve bir malzemenin ses absorpsiyon katsayısının belirlenmesi amacıyla
+                 geliştirilmiştir. Akustik Analiz Uygulamaları, ses ve titreşimlerin analizi ve değerlendirilmesi üzerine odaklanan bir bilim dalıdır. Bu analizler, genellikle akustik ölçümler, 
+                 veri toplama ve değerlendirme süreçlerini içeren, özel yazılım ve donanım sistemleri kullanılarak gerçekleştirilir. Akustik analizler, birçok alanda kritik öneme sahiptir; örneğin, bina akustiği, çevresel gürültü kontrolü, otomotiv endüstrisi, elektroakustik cihazlar ve tıbbi uygulamalar.
+Bu alandaki bir uygulama, ses yutma katsayısının belirlenmesi, yankı sürelerinin ölçülmesi ve ses yalıtım performanslarının değerlendirilmesi gibi işlemleri içerebilir. Akustik analizler, kaliteli ses ortamları yaratmak, gürültü kirliliğini azaltmak ve konforlu yaşam alanları sağlamak amacıyla kullanılır. 
+Ayrıca, bu analizler ile yapıların akustik özelliklerini optimize ederek, müzik ve konuşma performanslarını iyileştirmek mümkündür.
+                 """)
+        st.image('https://static.wixstatic.com/media/11062b_a9e50b12c27349e192c096a0bf39b18d~mv2.jpeg/v1/fill/w_1886,h_609,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/11062b_a9e50b12c27349e192c096a0bf39b18d~mv2.jpeg')
+        
+        
+    elif choice == "Test":
+        st.markdown("<h1 style='font-size: 28px;'>Test Sayfası <span style='font-size: 28px;'>📊</span></h1>", unsafe_allow_html=True)
+        uploaded_file = st.file_uploader(":file_folder: Veri dosyanızı buraya yükleyiniz. ",type=(["csv","txt","xlsx","xls"]))
+        data = None
+        if uploaded_file is not None:
+            data = pd.read_csv(uploaded_file)   
+            st.dataframe(data)
+        else:
+            st.warning("Lütfen bir veri dosyası yükleyin.")
+        
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            firma = st.text_input("Test Talep Eden/ Firma:")
+        with col2:
+            malzeme = st.text_input("Malzeme İsmi:")
+        with col3:
+            test_date = st.date_input("Test Tarihi", datetime.today())
+        
+        if st.button("Boş Kabin İçin T60 Değerleri"):
+            st.table(df_t60)
+        if st.button("Dolu Kabin İçin T60 Değerleri"):
+            st.table(df_t60_dolu)   
+            #yutum_katsayisi()
+
+        if st.button("Sonuçları Göster"): # bu butona basÄ±ldÄ±ÄÄ±nda kod Ã§alÄ±Åcak
+            if firma.strip() == "" or malzeme.strip() == "" or test_date is None or data is None:
+                st.error(":exclamation: Lütfen tüm alanlarÄ± doldurun.")
+            else:
+                #mainn()
+                yutum_katsayisi()
+                #generate_pdf(st.session_state.show_graph)
+                st.session_state.show_graph = True #sonuÃ§larÄ± gÃ¶ster butonuna basÄ±ldÄ±ÄÄ±nda true olarak dÃ¶ndÃ¼recek bu deÄiÅkeni
+        else:
+            st.session_state.show_graph = False #sonuÃ§larÄ± gÃ¶ster butonuna basÄ±lmamÄ±Åsa bu deÄiÅken false olcak
+        # grafikleri gÃ¶sterip gÃ¶stermemek iÃ§in yaptÄ±m bu deÄiÅkenleri
+
+        st.session_state.firma = firma
+        st.session_state.malzeme = malzeme
+        st.session_state.test_date = test_date
+
+
+    elif choice == "Test_Overall":
+        st.markdown("<h1 style='font-size: 28px;'>Test Sayfası <span style='font-size: 28px;'>📊</span></h1>", unsafe_allow_html=True)
+        test_option = st.radio("Test Seçeneğini Seçiniz:", ["Overall vs Time", "Overall vs RPM"])  
+        if test_option == "Overall vs Time":
+            st.write("Bu, Overall vs Time içeriğidir.")
+            # Buraya Test Seçeneği 1 ile ilgili kodlarınızı ekleyin
+        elif test_option == "Overall vs RPM":
+            st.write("Bu, Overall vs RPM içeriğidir.")
+            # Buraya Test Seçeneği 2 ile ilgili kodlarınızı ekleyin
+        uploaded_file = st.file_uploader(":file_folder: Veri dosyanızı buraya yükleyiniz. ",type=(["csv","txt","xlsx","xls"]))
+        data = None
+        if uploaded_file is not None:
+            data = pd.read_csv(uploaded_file)   
+            st.dataframe(data)
+        else:
+            st.warning("Lütfen bir veri dosyası yükleyin.")
+        
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            firma = st.text_input("Test Talep Eden/ Firma:")
+        with col2:
+            malzeme = st.text_input("Malzeme İsmi:")
+        with col3:
+            test_date = st.date_input("Test Tarihi", datetime.today())
+
+        st.session_state.firma = firma
+        st.session_state.malzeme = malzeme
+        st.session_state.test_date = test_date
+    
+
+    elif choice == "Rapor":
+        st.slider("What is your level",0,2000,step=50)
+        st.markdown("<h1 style='font-size: 28px;'>Rapor Sayfası <span style='font-size: 28px;'>📈</span></h1>", unsafe_allow_html=True)
+        st.write("Bu sayfa raporların garantilendiği bölümdür.")
+        st.write("Test Talep Eden/ Firma:", st.session_state.firma)
+        st.write("Malzeme İsmi:", st.session_state.malzeme)
+        st.write("Seçilen Deney Tarihi:", st.session_state.test_date)
+        #st.table(df_t60_dolu)
+        
+        if st.session_state.show_graph:  # Bu koÅul eklenerek T60 dolu tablosu kontrolÃ¼ yapÄ±lÄ±r
+            st.table(df_t60_dolu)
+            yutum_katsayisi()
+
+        if not st.session_state.show_graph: # sonuÃ§larÄ± gÃ¶ster butonuna basÄ±lmadan rapor sekmesine geÃ§ilirse
+            st.error(":exclamation: Test sayfasındaki işlemleri gerçekleştirmeden Rapor oluşturamazsınız.")
+        else:
+            generate_pdf(st.session_state.show_graph)
+
+            with open("rapor.pdf", "rb") as f:
+                st.download_button("Raporu PDF Olarak İndir", f.read(), file_name="rapor.pdf", key="pdf-download")
+
+if __name__ == "__main__":
+    main()  
+
+    
+
+
+    
