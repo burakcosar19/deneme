81f1ca800f307257d7741c1c605c01fda8f4fb97
orkunds <98600000+orkunds@users.noreply.github.com>
2025-02-10 03:51:32 +0300

    Update overall_plot.py

--- overall_plot.py
+++ overall_plot.py
@@ -6,25 +6,35 @@ Created on Mon Feb 10 02:41:14 2025
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 
-def overall_plott():
-    df=pd.read_excel("overallll.xlsx")
-    plt.figure(figsize=(16, 6))  # Grafiği geniş yap, yükseklik düşük olsun
-    plt.plot(df["Linear"], df["dB(A)"], linewidth=7)  # Çizgi grafiği
+def deneme_plot():
+    # Excel dosyasını oku
+    df = pd.read_excel("overallll.xlsx")
 
-    plt.title("Overall Level vs. Time ")
-    plt.ylabel("Ses Basınç Seviyesi [dB(A)]", fontsize=10)
-    plt.xlabel("Zaman (s)", fontsize=10)
+    # Şekil ve eksenleri oluştur
+    fig, ax = plt.subplots(figsize=(16, 6))
 
+    # Çizgi grafiğini oluştur
+    ax.plot(df["Linear"], df["dB(A)"], linewidth=6)
+
+    # Başlık ve eksen etiketlerini ayarla
+    ax.set_title("Overall Level vs. Time")
+    ax.set_ylabel("Ses Basınç Seviyesi [dB(A)]", fontsize=8)
+    ax.set_xlabel("Zaman (s)", fontsize=10)
 
     # X ekseni etiketlerini seyrekleştir
-    plt.xticks(np.arange(0, 45, step=0.75), rotation=90, fontsize=8)  # Her 5 birimde bir göster
-    plt.yticks(np.arange(0, 100, step=25), rotation=90, fontsize=10)
-    # Y eksenine grid ekle
-    plt.grid(axis='y', linestyle='--', alpha=0.5)
-    
-    st.pyplot()
+    ax.set_xticks(np.arange(0, df["Linear"].max(), step=0.75))  # X ekseni için 0.75 birimde bir göster
+    ax.set_xticklabels(ax.get_xticks(), rotation=90, fontsize=8)
+    ax.set_yticks(np.arange(0, 101, step=25))  # Y ekseni için 25 birimde bir göster
 
+    # Y eksenine grid ekle
+    ax.grid(axis='y', linestyle='--', alpha=0.5)
+
+    # Grafiği göster
+    plt.show(fig)
+
+if __name__ == "__deneme_plot__":
+    deneme_plot()
