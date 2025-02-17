17d1fb290e94272a250653d8fc58ca7df8fe4494
orkunds <98600000+orkunds@users.noreply.github.com>
2025-02-17 15:38:47 +0300

    Update overall_plot.py

--- overall_plot.py
+++ overall_plot.py
@@ -36,5 +36,32 @@ def deneme_plot():
     # Grafiği göster
     st.pyplot(fig)
 
+
+def deneme_plot1():
+    df = pd.read_excel("TPA_Ornek_Data_Analiz_OALevel_vs_RPM.xlsx")
+
+    # Şekil ve eksenleri oluştur
+    fig, ax = plt.subplots(figsize=(16, 6))
+
+    # Çizgi grafiğini oluştur
+    ax.plot(df["Linear"], df["dB(A)"], linewidth=6)
+
+    # Başlık ve eksen etiketlerini ayarla
+    ax.set_title("Overall Level vs. RPM")
+    ax.set_ylabel("Ses Basınç Seviyesi [dB(A)]", fontsize=8)
+    ax.set_xlabel("Zaman (s)", fontsize=10)
+
+    # X ekseni etiketlerini seyrekleştir
+    # X ekseni için 0.75 birimde bir göster
+    ax.set_xticks(np.arange(0, df["Linear"].max(), step=50))
+    ax.set_xticklabels(ax.get_xticks(), rotation=90, fontsize=8)
+    # Y ekseni için 25 birimde bir göster
+    ax.set_yticks(np.arange(0, 101, step=25))
+
+    # Y eksenine grid ekle
+    ax.grid(axis='y', linestyle='--', alpha=0.5)
+
+    st.pyplot(fig)
+
 if __name__ == "__deneme_plot__":
     deneme_plot()
